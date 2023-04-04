97970f357dcf0398507aa15c29be65fc7ac9134b
mhdsuhail172 <tjm.suhail172@gmail.com>
2023-04-04 15:46:56 -0400

    CompanyListings composable created

--- app/src/main/java/com/mhdsuhail/stonks/presentation/company_listings/CompanyListingScreen.kt
+++ app/src/main/java/com/mhdsuhail/stonks/presentation/company_listings/CompanyListingScreen.kt
@@ -1,8 +1,74 @@
 package com.mhdsuhail.stonks.presentation.company_listings
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Composable
-fun CompanyListingScreen(){
+@Destination(start = true)
+fun CompanyListingScreen(
+    navigator: DestinationsNavigator,
+    viewModel: CompanyListingsViewModel = hiltViewModel()
+) {
+
+    val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = viewModel.state.isRefreshing)
+
+    val state = viewModel.state
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        OutlinedTextField(
+            value = viewModel.state.searchQuery,
+            onValueChange = {
+                viewModel.onEvent(CompanyListingsEvents.SearchQueryChange(it))
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            placeholder = { Text("Search...") },
+            maxLines = 1,
+            singleLine = true
+        )
+
+        SwipeRefresh(state = swipeRefreshState, onRefresh = {
+            viewModel.onEvent(CompanyListingsEvents.Refresh)
+        }) {
+
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.companies.size) { i ->
+
+                    val company = state.companies[i]
+                    CompanyItem(
+                        company = company,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                // TODO - Navigate to details screen
+                            }
+                            .padding(16.dp)
+                    )
+                    if (i < state.companies.size) {
+                        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+                    }
+                }
+            }
+
+        }
+    }
 
 }
